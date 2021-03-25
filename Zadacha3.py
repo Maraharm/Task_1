1e46871e0dc7dd44f260146940424566edde9998
Maraharm <maraharmnika@gmail.com>
2021-03-25 22:21:56 +0500

    все равно не совсем так

--- Zadacha3.py
+++ Zadacha3.py
@@ -5,11 +5,40 @@
 как только прочитан символ «=», выводит результат и завершает работу.
 * Программа должна обрабатывать некорректный ввод.
 try:
-    stroka = str(input())
-    while stroka[0] != '=':"""
-s = str(input())
 
-print(eval(s))
+    while stroka[0] != '=':"""
+try:
+        pervoe = float(input())
+        while True:
+            dalee = input()
+            if dalee == '=':
+                print(pervoe)
+                break
+            if dalee in ('+', '-', '*', '/', '**', '//', '%'):
+                x = float(input())
+                if dalee == '+':
+                    pervoe =pervoe + x
+                elif dalee == '-':
+                    pervoe -= x
+                elif dalee == '**':
+                    pervoe **= x
+                elif dalee == '*':
+                    pervoe *= x
+                elif dalee == '//':
+                    pervoe //= x
+                elif dalee == '%':
+                    pervoe %= x
+                elif dalee == '/':
+                    pervoe /= x
+            else:
+                raise Exception
+except ValueError:
+        print("Это не число!")
+except ZeroDivisionError:
+        print("Нельзя делить на ноль")
+except Exception:
+        print("Некорректный ввод!")
+
 
 
 
